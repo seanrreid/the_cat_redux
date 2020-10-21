1d47f76caf5113c6ef97f21f40af15eaf65e9cb3
Sean R Reid <sean@torchcodelab.com>
2020-10-21 11:56:23 -0400

    Completed Exercise 1

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
+import { eat, play, nap } from "../redux/actions";
 
-const Activity = () => (
-    <>
-        <h1>What is the cat doing now???</h1>
-    </>
+const Activity = ({ activity, eat, play, nap }) => (
+  <>
+    <h1>What is the cat doing now???</h1>
+    <p>The cat is {activity}.</p>
+    <button type="button" onClick={() => eat()}>
+      Eating
+    </button>
+    <button type="button" onClick={() => play()}>
+      Playing
+    </button>
+    <button type="button" onClick={() => nap()}>
+      Napping
+    </button>
+  </>
 );
 
-export default Activity;
+const mapStateToProps = (state) => {
+  const { activity } = state;
+  return activity;
+};
+
+export default connect(mapStateToProps, { eat, play, nap })(Activity);
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,3 +1,4 @@
 import { combineReducers } from "redux";
+import activityReducer from "./activity";
 
-export default combineReducers({});
+export default combineReducers({ activity: activityReducer });
